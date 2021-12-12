117bd4e522903f87cfcb8d0741fb2aa89fe16bcb
YoheiIguchi <matthew.at.muse@gmail.com>
2021-12-12 13:26:55 +0900

    simple parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::num;
 use std::collections::BTreeMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct LexerError {
     pub msg: String,
 }
@@ -152,18 +152,104 @@ pub enum Token {
     Eof,
 }
 
+#[derive(Debug, Clone)]
+pub struct ParserError {
+    pub msg: String,
+}
+
+impl ParserError {
+    pub fn new(msg: &str) -> Self {
+        ParserError {
+            msg: msg.to_string(),
+        }
+    }
+}
+
 pub struct Parser {
     tokens: Vec<Token>,
+    index: usize,
 }
 
 impl Parser {
     pub fn new(tokens: Vec<Token>) -> Self {
-        let parser = Parser { tokens };
+        let parser = Parser { tokens, index: 0 };
         return parser;
     }
 
-    pub fn parse(&mut self) -> Value {
-        unimplemented!()
+    fn peek_token(&mut self) -> Result<&Token, ParserError> {
+        self.tokens
+            .get(self.index)
+            .ok_or_else(|| ParserError::new("error: a token isn't peekable"))
+    }
+
+    pub fn parse(&mut self) -> Result<Value, ParserError> {
+        let token = self.peek_token()?.clone();
+        let result = match token {
+            Token::Lbrace => self.parse_object(),
+            Token::String(s) => {
+                self.next_expect()?;
+                Ok(Value::String(s))
+            }
+            _ => Err(ParserError::new(&format!(
+                "error: a token must start {{ or [ or string or number or bool or null {:?}",
+                token
+            ))),
+        };
+        result
+    }
+
+    fn parse_object(&mut self) -> Result<Value, ParserError> {
+        let token = self.peek_token()?;
+        if *token != Token::Lbrace {
+            return Err(ParserError::new(&format!(
+                "error: JSON object must starts {{ {:?}",
+                token
+            )));
+        }
+
+        // skip {
+        self.next_expect()?;
+
+        let mut object = BTreeMap::new();
+        if *self.peek_token()? == Token::Rbrace {
+            return Ok(Value::Object(object));
+        }
+
+        loop {
+            let token1 = self.next_expect()?.clone();
+            let token2 = self.next_expect()?;
+            match (token1, token2) {
+                (Token::String(key), &Token::Colon) => {
+                    object.insert(key, self.parse()?);
+                }
+                _ => {
+                    return Err(ParserError::new(
+                        "error: a pair (key(string)) and : token) is required",
+                    ))
+                }
+            }
+            println!("{:?}", self.tokens);
+            println!("object {:?}, index: {}", object, self.index);
+            let token3 = self.next_expect()?;
+            match token3 {
+                Token::Rbrace => return Ok(Value::Object(object)),
+                Token::Comma => continue,
+                _ => {
+                    return Err(ParserError::new(&format!(
+                        "error: a {{, or token is expected: {:?}",
+                        token3
+                    )));
+                }
+            }
+        }
+    }
+
+    // get token from front and increment index
+    fn next_expect(&mut self) -> Result<&Token, ParserError> {
+        self.index += 1;
+        self.tokens
+            .get(self.index - 1)
+            .ok_or_else(|| ParserError::new("error: a token isn't peekable"))
     }
 }
 
@@ -247,12 +333,12 @@ mod test {
     fn test_parse() {
         let input = r#"
         {
-            "key": "value",
+            "key": "value"
         }
         "#;
         let l = Lexer::new(input).tokenize();
         let mut p = Parser::new(l);
-        let value = p.parse();
+        let value = p.parse().unwrap();
         let mut object = BTreeMap::new();
         object.insert("key".to_string(), Value::String("value".to_string()));
         assert_eq!(value, Value::Object(object));
